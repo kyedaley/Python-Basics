8deff1fd368ab7054f9cb6a7c8eb4f36dcf11a4b
Kye Daley <kyeboshdaley@gmail.com>
2021-10-31 16:21:40 +0000

    Add files via upload
    Basic equations and symbols you use in math with python.

---
+++ Calculating Sums.py
@@ -0,0 +1,29 @@
+# This is basic math equations what you may use in python
+
+# List of math symbols
+# + for addition
+# - subtraction
+# / division
+# * times by
+
+# % module division
+# Gets the remander from division
+
+# These are sometimes commonly uses
+
+# Lets make addition sum
+
+1stsum = int(input("Number 1:"))
+2ndsum = int(input("Number 2:"))
+
+print("Your totall sum is: ", 1stsum+2ndsum)
+
+# This is a basic sum working out.
+
+# Lets do division
+
+1stsum = int(input("Number 1:"))
+2ndsum = int(input("Number 2:"))
+
+print("Your totall sum is: ", 1stsum/2ndsum) # You see its changing 1 character
+#this will divide 1stsum by 2ndsum
